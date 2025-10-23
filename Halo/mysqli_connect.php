8308c572162ea597e5544870342c6f9c1f6c611f
simondlandau <76396809+simondlandau@users.noreply.github.com>
2025-10-23 14:44:12 +0100

    Clear database username and password
    Updated database connection credentials to empty strings.

--- Halo/mysqli_connect.php
+++ Halo/mysqli_connect.php
@@ -4,8 +4,8 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 $servername = "localhost";
-$username = "itmedia";
-$password = "send@AWS";
+$username = "";
+$password = "";
 $dbname = "Halo";
 
 // Create connection
